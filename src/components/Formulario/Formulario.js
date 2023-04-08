537ff51cb7c111f2726793c4ef0aa8033a214436
Walter Daniel Cuéllar Restrepo <102536695+DANIELDIM7@users.noreply.github.com>
2023-04-08 00:10:00 -0500

    Se crearon los primeros componentes, el header y la primera parte del form

---
+++ src/components/Formulario/Formulario.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Formulario.css'
+
+const Formulario = () => {
+  return (
+    <section className='formulario' >
+        <form>
+            <h2>Rellena el formulario para crear el colaborador.</h2>
+        </form>
+
+    </section>
+  )
+}
+
+export default Formulario
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Header.css'
+
+const Header = () => {
+  return (
+   <img src= '/img/Header.png'></img>
+  )
+};
+
+export default Header;
+
