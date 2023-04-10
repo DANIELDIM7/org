f1cce1b396bcf51d401a65e0ba2bf911abe680bd
Walter Daniel Cuéllar Restrepo <102536695+DANIELDIM7@users.noreply.github.com>
2023-04-10 13:41:17 -0500

    Se agregaron los estados para los primeros 3 inputs del formulario

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,12 +1,19 @@
-import React from 'react'
+
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
   
+
+  const manejarCambio = (e)=>{
+    props.actualizarValor(e.target.value) //Para que actualice el valor del estado con cada letra que se ingrese al input
+  }
+  
   return (
     <div className='campo-texto'>
       <label>{props.titulo}</label>
-      <input placeholder={props.placeholder} required={props.required}/>
+      <input placeholder={props.placeholder} required={props.required} value={props.valor}
+      onChange={manejarCambio}
+      />
     </div>
   )
 }
--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto";
 import ListaOpciones from "../ListaOpciones";
 import Boton from "../Boton";
 
 const Formulario = () => {
+  const [nombre, setNombre] = useState("");
+  const [puesto, setPuesto] = useState("");
+  const [foto, setFoto] = useState("");
 
-  const manejarEnvio = (event) =>{
+  const manejarEnvio = (event) => {
     event.preventDefault();
-    console.log('Manejar el envío',event);
-    
-  }
+    let datosAEnviar = {
+      nombre: nombre,
+      puesto: puesto,
+      foto: foto,
+    }
+    console.log(datosAEnviar);
+  };
   return (
     <section className="formulario">
       <form onSubmit={manejarEnvio}>
         <h2>Rellena el formulario para crear el colaborador.</h2>
-        <CampoTexto titulo="Nombre" placeholder="Ingresar el nombre" required={true} />
-        <CampoTexto titulo="Puesto" placeholder="Ingresar el puesto" required />
-        <CampoTexto titulo="Foto"   placeholder="Ingresar enlace de foto" />
-        <ListaOpciones/>
+        <CampoTexto
+          titulo="Nombre"
+          placeholder="Ingresar el nombre"
+          required={true}
+          valor={nombre}
+          actualizarValor={setNombre}
+        />
+        <CampoTexto
+          titulo="Puesto"
+          placeholder="Ingresar el puesto"
+          required
+          valor={puesto}
+          actualizarValor={setPuesto}
+        />
+        <CampoTexto
+          titulo="Foto"
+          placeholder="Ingresar enlace de foto"
+          valor={foto}
+          actualizarValor={setFoto}
+        />
+        <ListaOpciones />
         <Boton>Crear</Boton>
       </form>
     </section>
